e7d9153d26989d8b79a56185e3c48438a976cfb1
John Hartquist <john@sigmaiq.com>
2020-07-22 23:28:53 -0700

    first iteration of content

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,34 @@
 <template>
   <v-app>
-    <v-app-bar app color="black" dark flat>
-      <div class="d-flex align-center">
-        john hartquist
-      </div>
-    </v-app-bar>
-
     <v-main>
       <router-view />
     </v-main>
+    <v-footer color="white">
+      <v-container>
+        <v-row>
+          <v-col class="ml-4">
+            <v-divider width="600"></v-divider>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="py-0 ml-4">
+            <v-btn
+              v-for="btn in btns"
+              :key="btn.icon"
+              class="mr-4"
+              circle
+              fab
+              dark
+              small
+              elevation="0"
+              color="#ccc"
+            >
+              <v-icon>{{ btn.icon }}</v-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-footer>
   </v-app>
 </template>
 
@@ -19,7 +39,13 @@ export default Vue.extend({
   name: 'App',
 
   data: () => ({
-    //
+    btns: [
+      { icon: 'mdi-email' },
+      { icon: 'mdi-linkedin' },
+      { icon: 'mdi-twitter' },
+      { icon: 'mdi-github' },
+      { icon: 'mdi-facebook' },
+    ],
   }),
 })
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,82 @@
 <template>
-  <div class="home">
-    Coming soon...
-  </div>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card flat max-width="620">
+          <v-card-title class="text-h4 font-weight-bold">
+            John Hartquist
+          </v-card-title>
+
+          <v-card-subtitle class="text-h6 mb-2">
+            <p>Software Engineer</p>
+            <p>Seattle, WA</p>
+          </v-card-subtitle>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>Interests</v-list-item-title>
+              <v-list-item-subtitle
+                >Real-time Audio Analysis, VR/AR, Deep Learning, Functional
+                Programming, Reinforcement Learning, Prototyping
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>Preferred Languages</v-list-item-title>
+              <v-list-item-subtitle
+                >Python, TypeScript, C/C++, Scala</v-list-item-subtitle
+              >
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>Expertise</v-list-item-title>
+              <v-list-item-subtitle
+                >Deep Learning, Audio Programming, Full-Stack Web Development,
+                Test-Driven Development, Data Engineering, SQL, Data
+                Visualization, Distributed Systems, Serverless Computing,
+                Optimization, Parallel Programming</v-list-item-subtitle
+              >
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>Education</v-list-item-title>
+              <v-list-item-subtitle
+                >MSc. Computer Science '12<br />
+                California Polytechnic State University</v-list-item-subtitle
+              >
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>Experience</v-list-item-title>
+              <v-list-item-subtitle
+                ><a href="#">LinkedIn Profile</a><br />
+                <a href="#">Download Resume</a></v-list-item-subtitle
+              >
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>Blog Posts</v-list-item-title>
+              <v-list-item-subtitle>
+                <a href="#"
+                  >Audio Classification using FastAI and On-the-Fly Frequency
+                  Transforms</a
+                ></v-list-item-subtitle
+              >
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -9,3 +84,22 @@ export default {
   name: 'Home',
 }
 </script>
+
+<style lang="scss">
+.v-application {
+  .v-card__subtitle p {
+    margin-bottom: 0;
+  }
+  .v-list-item__subtitle {
+    white-space: normal;
+    a {
+      color: #999;
+      text-decoration: underline;
+      &:hover {
+        text-decoration: none;
+        color: #666;
+      }
+    }
+  }
+}
+</style>
